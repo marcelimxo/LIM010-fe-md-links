aeb80a351568d3cb189bbc3fd8d774396a73f031
marcelimxo <marcelimrondon@gmail.com>
2019-09-11 19:20:40 -0500

    :construction: TDD in progress :construction:
    All tests pass :white_check_mark:

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,13 +1,40 @@
-import { first, second } from '../lib/api/main.js';
+import { readPath, transformPath, readPathExt } from '../lib/api/main';
 
-describe('first', () => {
-  it('should be a function', () => {
-    expect(typeof first).toBe('function');
+const userPath = 'tests/data/prueba.md';
+const badPath = 'tests/none.txt';
+const absolutePath = '/home/laboratoria/Desktop/LIM010-fe-md-links/tests/data/prueba.md';
+
+describe('readPath ', () => {
+  it('should be a path prueba.md', () => {
+    expect(readPath(userPath)).toBe(true);
+  });
+
+  it('should not be a path (none.txt) because it does not exist', () => {
+    expect(readPath(badPath)).toBe(false);
+  });
+
+  it('should not be a path because is a string', () => {
+    expect(readPath('no soy un path')).toBe(false);
   });
 });
 
-describe('second', () => {
-  it('should be a function', () => {
-    expect(typeof second).toBe('function');
+
+describe('transformPath', () => {
+  it('should be a absolute path', () => {
+    expect(transformPath(userPath)).toBe(absolutePath);
+  });
+
+  it('should be the same path', () => {
+    expect(transformPath(absolutePath)).toBe(absolutePath);
+  });
+});
+
+describe('readPathExt', () => {
+  it('should be .md', () => {
+    expect(readPathExt(userPath)).toBe(true);
+  });
+
+  it('should be a .txt', () => {
+    expect(readPathExt(badPath)).toBe(false);
   });
 });
