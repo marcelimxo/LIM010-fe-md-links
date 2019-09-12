1395d0d3fb1d298e93d977306e4e9307a1ef38a8
marcelimxo <marcelimrondon@gmail.com>
2019-09-12 16:17:33 -0500

    :heavy_plus_sign: Returning paths inside a dir

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,11 @@
-import { readPath, transformPath, readPathExt } from '../lib/api/main';
+import {
+  readPath, transformPath, readPathExt, pathFiles,
+} from '../lib/api/main';
 
 const userPath = 'tests/data/prueba.md';
 const badPath = 'tests/none.txt';
 const absolutePath = '/home/laboratoria/Desktop/LIM010-fe-md-links/tests/data/prueba.md';
+const dirPath = '/home/laboratoria/Desktop/LIM010-fe-md-links/tests/data';
 
 describe('readPath ', () => {
   it('should be a path prueba.md', () => {
@@ -36,5 +39,19 @@ describe('readPathExt', () => {
 
   it('should be a .txt', () => {
     expect(readPathExt(badPath)).toBe(false);
+  });
+});
+
+describe('pathFiles', () => {
+  it('should return an array with the files inside a directory', () => {
+    expect(Array.isArray(pathFiles(dirPath))).toBe(true);
+  });
+
+  it('should return an array with the file', () => {
+    expect(Array.isArray(pathFiles(userPath))).toBe(true);
+  });
+
+  it('should return an array with the file name', () => {
+    expect(pathFiles(userPath)).toEqual([userPath]);
   });
 });
