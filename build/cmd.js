5fa40af2177181ab49c3b08f6533366c9edcd682
marcelimxo <marcelimrondon@gmail.com>
2019-09-25 18:58:29 -0500

    :arrows_counterclockwise: Updating cli

--- build/cmd.js
+++ build/cmd.js
@@ -9,15 +9,19 @@ var _index = _interopRequireDefault(require("./index"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/* eslint-disable no-console */
+/* eslint-disable no-param-reassign */
 const chalk = require('chalk');
 
 const emoji = require('node-emoji');
 
 const {
-  blue
+  blue,
+  red,
+  green,
+  magenta,
+  white,
+  cyan
 } = chalk.bold;
-const pink = chalk.hex('#ff94ea');
 
 const extraerParam = (extendido, resumido) => (param1, param2) => param1 === extendido || param2 === extendido || param1 === resumido || param2 === resumido;
 
@@ -25,34 +29,49 @@ const extraerValidate = extraerParam('--validate', '-v');
 const extraerStats = extraerParam('--stats', '-s');
 
 var _default = (ruta, param1, param2) => new Promise(resolve => {
-  const validate = extraerValidate(param1, param2);
-  const stats = extraerStats(param1, param2);
-  (0, _index.default)(ruta, {
-    validate
-  }).then(links => {
-    if (stats) {
-      resolve(`${emoji.get(':bar_chart:')}   ${blue(`hay ${links.length} links`)}`);
+  if (!ruta) {
+    resolve(`${emoji.get(':no_entry:')}   ${red('Por favor, ingresa una ruta')} `);
+  } else {
+    const validate = extraerValidate(param1, param2);
+    const stats = extraerStats(param1, param2);
+    (0, _index.default)(ruta, {
+      validate
+    }).then(objLinks => {
+      const validatedLinks = objLinks.map(link => {
+        if (link.status === 200) {
+          link.ok = `${green('OK')}`;
+          link.status = `${green(link.status)}`;
+        } else {
+          link.ok = `${red('FAIL')}`;
+          link.status = `${red(400)}`;
+        }
 
-      if (validate) {
-        // stats y validate
-        console.log('hola stats y validate');
-      } else {
-        // validate
-        console.log('hola validate');
-      }
-    } else {
-      const arrLinks = [];
-      links.forEach(element => {
-        const linkArr = Object.values(element);
+        return Object.values(link);
+      }); // Creo un array de strings donde estan los valores del objeto link
+
+      const links = [];
+      const uniqueLinks = new Set(objLinks.map(link => link.href));
+      const badLinks = new Set(objLinks.map(link => link.status !== 200)); // TODO: Cambiar a map
+
+      objLinks.forEach(eachLink => {
+        const linkArr = Object.values(eachLink);
         const linkStr = linkArr.join(' ');
-        arrLinks.push(linkStr);
+        links.push(linkStr);
       });
-      resolve(`${emoji.get(':link:')}  ${pink(arrLinks.join(`\n${emoji.get(':link:')}  `))}`);
-    }
-  }).catch(err => {
-    resolve(err);
-  });
-}); // cli(ruta, param1, param2).then(console.log);
 
+      if (stats && validate) {
+        resolve(`${emoji.get(':bar_chart:')}   ${blue(`Total: ${links.length}`)} \n${emoji.get(':chart_with_upwards_trend:')}   ${green(`Unique: ${uniqueLinks.size}`)}\n${emoji.get(':chart_with_downwards_trend:')}   ${magenta(`Broken: ${badLinks.size}`)}`);
+      } else if (stats) {
+        resolve(`${emoji.get(':bar_chart:')}   ${blue(`Total: ${links.length}`)} \n${emoji.get(':chart_with_upwards_trend:')}   ${green(`Unique: ${uniqueLinks.size}`)}`);
+      } else if (validate) {
+        resolve(`${emoji.get(':link:')}  ${white(links.join(`\n${emoji.get(':link:')}  `))}`);
+      } else {
+        resolve(`${emoji.get(':link:')}  ${white(validatedLinks.join(`\n${emoji.get(':link:')}  `))}`);
+      }
+    }).catch(() => {
+      resolve('hay un error');
+    });
+  }
+});
 
 exports.default = _default;
